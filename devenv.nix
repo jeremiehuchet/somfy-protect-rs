5692f3cf028f437e4cbc2d024fc1fa449b7193c0
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2024-05-22 00:25:13 +0200

    👷 Setup devenv

---
+++ devenv.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  packages = [
+    pkgs.openapi-generator-cli
+    pkgs.openssl
+    pkgs.pkg-config
+    pkgs.vscode
+  ];
+
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+  };
+
+  pre-commit.hooks.rustfmt.enable = true;
+
+  scripts.docker-build-mock.exec = ''
+    docker build \
+        --file $DEVENV_ROOT/mock/Dockerfile \
+        --tag somfy-protect-api-mock:latest \
+        $DEVENV_ROOT/mock
+  '';
+  scripts.openapi-generate.exec = ''
+    openapi-generator-cli generate \
+        -g rust \
+        -o $DEVENV_ROOT/somfy-protect-openapi \
+        -i $DEVENV_ROOT/somfy-protect-openapi/somfy-protect-openapi.json \
+        -c $DEVENV_ROOT/somfy-protect-openapi/openapi-config.yml
+  '';
+}
